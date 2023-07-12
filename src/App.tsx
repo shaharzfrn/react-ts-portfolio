d97fd0b66fb8a1adb9fb265e70afb34aa3027e23
Shahar. Z <Shaharzfrn@gmail.com>
2023-07-12 14:47:20 +0300

    react-ts-protfolio init

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useEffect } from 'react';
+import { Col, Container, Row, Navbar } from 'react-bootstrap';
+import Welcome from './components/Welcome';
+
+import './App.css';
+
+function App() {
+  return <h1>Hello World</h1>;
+}
+
+export default App;

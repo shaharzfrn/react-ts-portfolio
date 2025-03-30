96435b65d7cb9e08524d8c937368d835b38dcc00
Shahar. Z <Shaharzfrn@gmail.com>
2025-03-30 13:53:47 +0300

    update resume

--- src/assets/data/resume/experience.tsx
+++ src/assets/data/resume/experience.tsx
@@ -1,66 +1,87 @@
 const data = [
-    {
-        name: 'SkySoft Solutions',
-        position: 'Software Engineer',
-        url: 'https://www.linkedin.com/company/skysoft-solutions-by-comm-it/mycompany/verification/',
-        startDate: '2024-03-03',
-        endDate: undefined,
-        summary: (
-            <>
-                Embedded Software Engineer
-            </>
-        ),
-        highlights: [
-            <>Develop & Maintain Embedded C/C++ systems</>,
-            <>
-                <a href="https://www.qt.io/product/framework" target="_blank" rel="noreferrer">
-                    Qt Framework
-                </a>
-            </>,
-        ],
-    },
-    {
-        name: 'Freelancer',
-        position: 'Full-Stack Developer',
-        url: '',
-        startDate: '2019-06-01',
-        endDate: '2024-03-03',
-        summary: (
-            <>
-                Full-Stack Developer freelancer. write softwares from scratch, continued
-                work on exists code and fixing bugs.
-            </>
-        ),
-        highlights: [
-            <>
-                Work mainly on <a href="https://www.fiverr.com/shaharzfrn">Fiverr</a>{' '}
-                with client all over the world.
-            </>,
-            <>Most of my programming works was in Python, C++ and Java.</>,
-            <>All of my IT work was on Linux (mainly servers).</>,
-            <>
-                Work on small project in{' '}
-                <a href="https://www.walkme.com" target="_blank" rel="noreferrer">
-                    walkme
-                </a>
-                .
-            </>,
-        ],
-    },
-    {
-        name: 'Israel Defense Force',
-        position: 'System Administrator',
-        url: '',
-        startDate: '2016-08-01',
-        endDate: '2018-08-01',
-        summary: <>System Administrator, Unit Matzov</>,
-        highlights: [
-            <> VMWare ESXi </>,
-            <> NetApp </>,
-            <> Microsoft Servers include MS Exchange </>,
-            <> Cisco & CheckPoint </>
-        ],
-    },
+  {
+    name: 'SkySoft Solutions by Comm-it',
+    position: 'Embedded Software Engineer',
+    url: 'https://www.linkedin.com/company/skysoft-solutions-by-comm-it/mycompany/verification/',
+    startDate: '2024-03-03',
+    endDate: undefined,
+    summary: (
+      <>
+        Developing and maintaining complex C++ real-time embedded applications
+        while working on multiple projects in parallel. <br />
+        Collaborating with in-house and third-party teams to fast and reliable
+        integrating new features while focusing on performance optimization and
+        multi-threaded programming. <br />
+        Using reverse engineering complex systems to identify and functionality,
+        and ensure system compatibility, all while while adhering to security
+        and confidentiality standards.
+      </>
+    ),
+    highlights: [
+      <>
+        Use my knowledge and experience in Reverse-Engineering to recover lost
+        source codes from binary files.
+      </>,
+      <>Solve complex problems and critical bugs in veruse topics</>,
+      <>
+        Integrate Arcgis WMS maps from GeoServer into a key company application.
+      </>,
+      <>
+        Delivered quick and reliable POCs for new features during the design
+        stage.
+      </>,
+    ],
+  },
+  {
+    name: 'Freelancer',
+    position: 'Full-Stack Developer',
+    URL: '',
+    startDate: '2019-06-01',
+    endDate: '2024-03-03',
+    summary: (
+      <>
+        Worked on <a href="https://www.fiverr.com/shaharzfrn">Fiverr</a> and
+        have a rate of 5/5.
+        <br />
+        Built applications from scratch and continued existing projects for
+        clients worldwide.
+        <br />
+        Managed the full development lifecycle, from design to deployment.
+      </>
+    ),
+    highlights: [
+      <>Build GUI application from scratch using QT & Tkinter</>,
+      <>
+        Work on significate project with{' '}
+        <a href="https://www.walkme.com" target="_blank" rel="noreferrer">
+          walkme
+        </a>
+        .
+      </>,
+    ],
+  },
+  {
+    name: 'IDF - Matzov',
+    position: 'System Administrator',
+    URL: '',
+    startDate: '2016-08-01',
+    endDate: '2018-08-01',
+    summary: (
+      <>
+        Served as a System Administrator in the National Center of Encryption
+        and Information Security, building and deploying operational systems
+        used across all IDF units, ensuring their reliability and performance.
+        Contributed to the integration and deployment of new systems and
+        technologies to enhance operational capabilities.
+      </>
+    ),
+    highlights: [
+      <> VMWare ESXi </>,
+      <> NetApp </>,
+      <> Microsoft Servers & MS Exchange </>,
+      <> Cisco & CheckPoint </>,
+    ],
+  },
 ];
 
 export default data;
